a74181d76045ff4fc2a324a7c74e8778d578804f
R. Abid Hermawan <97014247+rabidhermawan@users.noreply.github.com>
2025-10-28 15:21:08 +0700

    5 - Getting User Input to Book Ticket Logic

--- main.go
+++ main.go
@@ -4,10 +4,9 @@ import "fmt"
 
 func main() {
 	// var conferenceName string = "Go Conference"
-	conferenceName := "Go Conference"
-
+	conferenceName := "Go Conference" // This can't work if we are declaring constant or explicitly defining a type
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -15,11 +14,26 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// Ask user for their name
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	// Ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
