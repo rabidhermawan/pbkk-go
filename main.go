c58a58786511b35b345e16409cd5e1ed140a8672
R. Abid Hermawan <97014247+rabidhermawan@users.noreply.github.com>
2025-10-28 15:33:33 +0700

    6 - Arrays

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ func main() {
 	conferenceName := "Go Conference" // This can't work if we are declaring constant or explicitly defining a type
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings [50]string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// var bookings = [50]string{"Nana", "Nicole", "Peter"}
 
 	var firstName string
 	var lastName string
@@ -33,6 +36,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
